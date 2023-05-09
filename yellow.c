ea35bc578e8f6eaed5b04b08fb5b3400c828fe5f
pk <fakemoney600@gmail.com>
2023-05-09 00:42:29 -0600

    Update yellow.c

--- yellow.c
+++ yellow.c
@@ -1,6 +1,8 @@
 #include "raylib.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
 
 #define MAX_BALLS 50
 
@@ -24,69 +26,139 @@ int main(void)
 
    Particle particles[MAX_BALLS] = {0};
    Vector2 mousePos = {0};
+    
 
+    Rectangle rect = { 50, 50, 70, 40};
+    char* text = "water";
+    Color textColor = {0, 0, 190, 100};
+    Color rectColor = {50, 0, 255, 100};
+    
+    // .   50, 50, 70, 40, (Color){0, 0, 190, 100}
+ 
    bool mouseClicked = false;
+
+   bool selectMode = false;
+   bool shapeDrawn = false;
+   Rectangle selectRect = {0};
+   Vector2 startPoint = { 0 };
+   Vector2 endPoint = { 0 };
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
+
+
+
+
+
+
+
+
+
+
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    while (!WindowShouldClose()) {   // Detect window close button or ESC key
+    
+        
+        if(IsKeyPressed(KEY_B)){
+            selectMode = true;         
+			shapeDrawn = false;
+        }
+
+        if(selectMode){
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                startPoint = GetMousePosition();
+            }
+            if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+                endPoint = GetMousePosition();
+
+                selectRect.x = fminf(startPoint.x, endPoint.x);
+                selectRect.y = fminf(startPoint.y, endPoint.y);
+                selectRect.width = fabsf(endPoint.x - startPoint.x);
+                selectRect.height = fabsf(endPoint.y - startPoint.y);
+
+                shapeDrawn = true;
+            } else if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+                selectMode = false;
+            }
+        }
         // updated mouse input
         mouseClicked = IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
         mousePos = GetMousePosition();
 
 
-        //SPAWN THE PARTICLES
-        if(mouseClicked){
-            for(int i = 0; i < MAX_BALLS; i++){
-                if(!particles[i].active){
-                    particles[i].active = true;
-                    particles[i].position = mousePos;
-                    particles[i].velocity.x = (float)GetRandomValue(-100, 100) / 50.0f;
-                    particles[i].velocity.y = (float)GetRandomValue(-100, 100) / 50.0f;
-                    particles[i].radius = (float) GetRandomValue(1, 10);
-                    particles[i].color = (Color){250, 225, 201, 255};
-
-                }
-            }
-        }
-
-        //Update particle LifeTime and Position
+        
+        //SPAWN THE PARTICLES WHEN 'P' KEY IS PRESSED
+        if(IsKeyPressed(KEY_P)){
         for(int i = 0; i < MAX_BALLS; i++){
-            if (particles[i].active) {
-                particles[i].position.x += particles[i].velocity.x;
-                particles[i].position.y += particles[i].velocity.y;
-                particles[i].radius -= 0.05f;
+            if(!particles[i].active){
+                particles[i].active = true;
+                particles[i].position = mousePos;
+                particles[i].velocity.x = (float)GetRandomValue(-100, 100) / 50.0f;
+                particles[i].velocity.y = (float)GetRandomValue(-100, 100) / 50.0f;
+                particles[i].radius = (float) GetRandomValue(1, 10);
 
-                if(particles[i].radius <= 0){
-                    particles[i].active = false;
+                int colorIndex = GetRandomValue(0, 2);
+                switch(colorIndex){
+                    case 0:
+                        particles[i].color = (Color){255, 255, 137, 255}; // #ffff89
+                        break;
+                    case 1:
+                        particles[i].color = (Color){227, 51, 30, 255}; // #e3331e
+                        break;
+                    case 2:
+                        particles[i].color = (Color){18, 10, 2, 255}; // #120a02
+                        break;
                 }
+
             }
         }
+    }
+
+    //Update particle LifeTime and Position
+        for(int i = 0; i < MAX_BALLS; i++){
+        if (particles[i].active) {
+            particles[i].position.x += particles[i].velocity.x;
+            particles[i].position.y += particles[i].velocity.y;
+            particles[i].radius -= 0.05f;
+
+            if(particles[i].radius <= 0){
+                particles[i].active = false;
+            }
+        }
+    }
+
+    // ... Rest of the code ...
+
+
+
+
 
         
         BeginDrawing();
 
-            //Draw Particle
-            for (int i = 0; i < MAX_BALLS; i++) {
-            if (particles[i].active) {
-                DrawCircleV(particles[i].position, particles[i].radius, particles[i].color);
+
+            if(shapeDrawn) {
+                DrawRectangleRec(selectRect, (Color){255, 0, 0, 100});
             }
-        }
-
-
-            
+            /*
+            else if(selectMode) {
+                DrawRectangleLinesEx(selectRect, 2, RED);
+            }
+            */
             ClearBackground(LIGHTGRAY);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-            //DrawCircleV(ballPos, 20, MAROON);
+            //Draw Particle
+            for (int i = 0; i < MAX_BALLS; i++) {
+                if (particles[i].active) {
+                    DrawCircleV(particles[i].position, particles[i].radius, particles[i].color);
+                }
+            }
 
-            //if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-            // ballPos = GetMousePosition();  
-            //}
-
+            DrawText("Press: P for Particles, Press B for Box Select", 190, 200, 20, WHITE);
+            //water
+            
+           DrawRectangle(50, 50, 70, 40, textColor);
             
 
         EndDrawing();
@@ -95,8 +167,9 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+          // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
-
-    return 0;
 }
+
+
+                        
